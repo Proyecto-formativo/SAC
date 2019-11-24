c07f16286f78122a403bc39e366f8aee33412a9d
frayxyz <55639860+frayxyz@users.noreply.github.com>
2019-11-24 00:34:37 -0500

    menu reportes

--- application/views/content/Coordinador/menu.php
+++ application/views/content/Coordinador/menu.php
@@ -1,5 +1,9 @@
-<div class="row" >
-	<div class="" style="height: 70px; background: #F1C40F; width: 96.2%; margin: auto;margin-bottom: 10px;">hola desde menu</div>
+<div class="row"   style="height: 50px; width: 100%; margin: auto;margin-bottom: 10px; text-transform:uppercase; text-align:center; uppercase; color: #fcfcfc">
+
+		<div class="col-4" style=" border:2px solid #fff; background: #249762;"><h2> nuevos</h2> </div>
+	<div class="col-4" style=" border:2px solid #fff; background: #249762;"><h2>aprovados</h2></div>
+	<div class="col-4" style=" border:2px solid #fff; background: #249762;"><h2>vistos</h2></div>
+
 </div>
 
 
